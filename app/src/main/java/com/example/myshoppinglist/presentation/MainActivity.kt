bd14c82b021f42b66284acf734398434a43eaa3c
Azat Azmetov <azmetoff@gmail.com>
2021-12-05 22:13:39 +0300

    use linear layout instead of recycler view... huh

--- app/src/main/java/com/example/myshoppinglist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/myshoppinglist/presentation/MainActivity.kt
@@ -1,28 +1,48 @@
 package com.example.myshoppinglist.presentation
 
 import android.os.Bundle
-import android.util.Log
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.myshoppinglist.R
+import com.example.myshoppinglist.domain.ShopItem
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
+    private lateinit var llshoplist: LinearLayout
     private var count = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        llshoplist = findViewById(R.id.ll_shop_list)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-
         viewModel.shopList.observe(this) {
-            Log.d("MainActivity", it.toString())
-            if (count == 0) {
-                count++
-                val item = it[0]
-                viewModel.changeEnableState(item)
+            showList(it)
+        }
+    }
+
+    private fun showList(list: List<ShopItem>) {
+        llshoplist.removeAllViews()
+        for (shopItem in list) {
+            val layoutId = if (shopItem.enabled) {
+                R.layout.item_shop_enabled
+            } else {
+                R.layout.item_shop_enabled
             }
+            val view = LayoutInflater.from(this).inflate(layoutId, llshoplist, false)
+            val tvName = view.findViewById<TextView>(R.id.tv_name)
+            val tvCount = view.findViewById<TextView>(R.id.tv_count)
+            tvName.text = shopItem.name
+            tvCount.text = shopItem.count.toString()
+            view.setOnLongClickListener {
+                viewModel.changeEnableState(shopItem)
+                true
+            }
+            llshoplist.addView(view)
         }
 
 
