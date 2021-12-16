1c5dac2874da301b1171c3c136d203564c79ccba
ztzmtv <azmetoff@gmail.com>
2021-12-16 22:09:19 +0300

    fixed mistake with adding to gitignore

---
+++ app/src/main/java/com/example/myshoppinglist/domain/EditShopItemUseCase.kt
@@ -0,0 +1,8 @@
+package com.example.myshoppinglist.domain
+
+class EditShopItemUseCase(private val shopListRepository: ShopListRepository) {
+
+    fun editShopItem(shopItem: ShopItem) {
+        shopListRepository.editShopItem(shopItem)
+    }
+}
